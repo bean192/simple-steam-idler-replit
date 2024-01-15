8ffb4a4535eaed73c5aa2bf1c8cb587a22dfb4dd
bean192 <75428286+bean192@users.noreply.github.com>
2024-01-15 20:22:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 221100, 990080];  // Enter here AppIDs of the needed games
+var games = [730, 221100];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
